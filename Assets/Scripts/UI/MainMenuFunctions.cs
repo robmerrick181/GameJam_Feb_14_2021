504350d61d626b03b1b275e7b0fe639aaa40ff32
Spenny-J <jensenrspencer@gmail.com>
2021-02-15 15:24:49 -0800

    Gave the settings menu functionality

--- Assets/Scripts/UI/MainMenuFunctions.cs
+++ Assets/Scripts/UI/MainMenuFunctions.cs
@@ -2,9 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuFunctions : MonoBehaviour
 {
+
+    public void VolumeSlider(float _val)
+    {
+        print(_val);
+    }
+
+    public void FullScreen(int _val)
+    {
+        if (_val == 0)
+            Screen.fullScreen = true;
+        else
+            Screen.fullScreen = false;
+    }
+
     public void OpenURL(string _url)
     {
         Application.OpenURL(_url);
