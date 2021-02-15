cda41c7cb0af97b22c595b57f0a9e538fc6de6e3
Rob Merrick <robmerrick181@gmail.com>
2021-02-15 11:57:48 -0700

    Adding in fundamental 3rd person controls and camera operations
    Adding in fundamental 3rd person controls and camera operations

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] [Range(0.0001F, 0.05F)] private float _strafingSensitivity = 0.02F;
+
+    private Rigidbody _rigidBody;
+
+	private void Start()
+	{
+		_rigidBody = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+    {
+        UpdateStrafing();
+        UpdateJump();
+    }
+
+	private void UpdateStrafing()
+	{
+        Vector3 translation = Vector3.zero;
+        Vector3 cameraForward = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
+        Vector3 cameraBackward = -cameraForward;
+        Vector3 cameraRight = Camera.main.transform.right;
+        Vector3 cameraLeft = -cameraRight;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            translation += cameraForward;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            translation += cameraLeft;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            translation += cameraBackward;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            translation += cameraRight;
+        }
+
+        _rigidBody.MovePosition(_rigidBody.position + translation.normalized * _strafingSensitivity);
+    }
+
+    private void UpdateJump()
+	{
+        if(Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(_rigidBody.velocity.y) < Mathf.Epsilon)
+		{
+            _rigidBody.AddForce(6.5F * Vector3.up, ForceMode.Impulse);
+		}
+	}
+}
