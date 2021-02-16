ec0b6f83b732dbb4844d9db2567f1325b00184bd
Spenny-J <jensenrspencer@gmail.com>
2021-02-15 18:47:19 -0800

    Finished Consumable Spawner

--- Assets/Scripts/Consumables/ConsumableSpawner.cs
+++ Assets/Scripts/Consumables/ConsumableSpawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ConsumableSpawner : MonoBehaviour
 {
+    [SerializeField] private LayerMask GroundLayer;
+
     [SerializeField] private Vector3 TriggerSize = new Vector3(15, 15, 15);
     private Vector3 DebugBoxSize = Vector3.zero;
 
@@ -11,6 +13,53 @@ public class ConsumableSpawner : MonoBehaviour
     [SerializeField] private GameObject Bow;
     [SerializeField] private GameObject HealthPotion;
     [SerializeField] private GameObject StrengthPotion;
+
+    private Coroutine swordSpawnTimer;
+    private Coroutine bowSpawnTimer;
+    private Coroutine healthSpawnTimer;
+    private Coroutine strengthSpawnTimer;
+
+    private int SpawnAttempts = 0;
+
+    public bool CanSpawn = false;
+
+    private void Update()
+    {
+        if (swordSpawnTimer == null)
+            swordSpawnTimer = StartCoroutine(SpawnItemTimer(Sword, 60, 120));
+        if (bowSpawnTimer == null)
+            bowSpawnTimer = StartCoroutine(SpawnItemTimer(Bow, 60, 120));
+        if (healthSpawnTimer == null)
+            healthSpawnTimer = StartCoroutine(SpawnItemTimer(HealthPotion, 5, 60));
+        if (strengthSpawnTimer == null)
+            strengthSpawnTimer = StartCoroutine(SpawnItemTimer(StrengthPotion, 5, 60));
+    }
+
+    private Vector3 CreateSpawnLocation()
+    {
+        if(SpawnAttempts > 5)
+        {
+            Debug.LogError("Unable to spawn item. Quitting loop.");
+            return Vector3.zero;
+        }
+
+        Vector3 _spawnLocation = new Vector3(transform.position.x + Random.Range(-TriggerSize.x, TriggerSize.x), 
+            transform.position.y, transform.position.z + Random.Range(-TriggerSize.z, TriggerSize.z));
+
+        //Raycast down from this position
+        RaycastHit hit;
+        if (Physics.Raycast(_spawnLocation, -transform.up, out hit, TriggerSize.y, GroundLayer, QueryTriggerInteraction.Ignore))
+        {
+            _spawnLocation.y = hit.transform.position.y;
+            SpawnAttempts = 0;
+            return _spawnLocation;
+        }
+        else
+        {
+            SpawnAttempts++;
+            return CreateSpawnLocation();
+        }
+    }
 
     private void SpawnItem(Vector3 spawnLocation, GameObject obj)
     {
@@ -22,7 +71,18 @@ public class ConsumableSpawner : MonoBehaviour
         else
             StartCoroutine(RetrySetActive(item));
     }
+    private IEnumerator SpawnItemTimer(GameObject _obj, float _lowestSpawnTime, float _longestSpawnTime)
+    {
+        Vector3 spawnPos = CreateSpawnLocation();
 
+        float waitTime = Random.Range(_longestSpawnTime, _longestSpawnTime);
+        yield return new WaitForSeconds(waitTime);
+
+        if (spawnPos != Vector3.zero)
+            SpawnItem(spawnPos, _obj);
+        else
+            Debug.LogError("Unable to spawn item: Location is: " + spawnPos);
+    }
     private IEnumerator RetrySetActive(GameObject obj)
     {
         yield return new WaitForSeconds(1f);
