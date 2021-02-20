d4588b5ad93c032dedb15bef14953db1d54ef376
Rob Merrick <robmerrick181@gmail.com>
2021-02-20 13:58:13 -0700

    Making further progress on Boss AI
    Making further progress on Boss AI

--- Assets/Scripts/BossAI/BossAIActionDecision.cs
+++ Assets/Scripts/BossAI/BossAIActionDecision.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class BossAIActionDecision : StateMachineBehaviour
 {
+	private Character _bossCharacter;
+	private Character _playerCharacter;
+
 	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
+		_bossCharacter = animator.GetComponentInParent<Character>();
 
+		if(_bossCharacter.IsDead)
+		{
+			 
+		}
 	}
 
 	public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
---
+++ Assets/Scripts/BossAI/BossAIHelper.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAIHelper
+{
+	public Character BossCharacter { get; }
+	public Character PlayerCharacter { get; }
+
+	public BossAIHelper()
+	{
+
+	}
+}
